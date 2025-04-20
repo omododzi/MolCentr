80a29d03e625f4a5154969ad8dc47cba2dd7996b
omododzi <83458772+omododzi@users.noreply.github.com>
2025-04-20 12:23:09 +0300

    fix

--- Assets/scripts/ButtontoBY.cs
+++ Assets/scripts/ButtontoBY.cs
@@ -1,28 +1,39 @@
 using System;
 using UnityEngine;
+using TMPro;
 
 public class ButtontoBY : MonoBehaviour
 {
-    public GameObject[] obj;
+    public GameObject[] CubeorDecor;
     public GameObject[] buttons;
     public GameObject floor;
     public GameObject spawnfloor;
     private Spawnguest _spawn;
+    public TMP_Text text;
+    public int summBY;
+
+    public GameObject Preftospawn;
+    public GameObject Cube;
 
     void Start()
     {
         _spawn = new Spawnguest();
     }
 
+    void Update()
+    {
+        text.text = summBY + "$";
+    }
+
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            if (obj != null)
+            if (CubeorDecor != null)
             {
-                for (int i = 0; i < obj.Length; i++)
+                for (int i = 0; i < CubeorDecor.Length; i++)
                 {
-                    obj[i].SetActive(true);
+                    CubeorDecor[i].SetActive(true);
                 }
             }
             if (buttons != null)
@@ -38,6 +49,12 @@ public class ButtontoBY : MonoBehaviour
                 Instantiate(floor, spawnfloor.transform.position, Quaternion.identity);
                 _spawn.SpawnGuests();
             }
+
+            if (Cube != null && Preftospawn != null)
+            {
+                Instantiate(Preftospawn,Cube.transform.position,Quaternion.identity);
+            }
+            Destroy(gameObject);
         }
     }
 }
